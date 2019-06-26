97673065fca86fbe168b9b0ec36b53ce3f715d7c
Jeongpil Jang <torra3167@ekay.co.kr>
2019-06-26 16:29:48 +0900

    test

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import com.example.demo.Service.EmployeeService;
 import com.example.demo.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -16,6 +13,12 @@ public class EmployeeController {
 
     @Autowired
     EmployeeService empService;
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+
+        return "hello!!";
+    }
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
     public List<Employee> getEmployees() {
